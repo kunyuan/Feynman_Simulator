a93f65a6b3cbb6b332b108efd0072c8ad80c7ae6
huangy22 <huangy22@users.noreply.github.com>
2014-10-15 14:53:09 -0400

    add Lattice Plot and tests

--- src/lattice/lattice.cpp
+++ src/lattice/lattice.cpp
@@ -9,6 +9,7 @@
 #include "lattice.h"
 #include "math.h"
 #include "convention.h"
+#include "cnpy.h"
 #include <iostream>
 
 Lattice lattice;
@@ -40,10 +41,10 @@ void Lattice::Initialize()
 //    LatticeVec[0][1]=1.0;
 //    
 //    LatticeVec[1][0]=sqrt(3.0)/2.0;
-//    LatticeVec[1][1]=0.5;
+//    LatticeVec[1][1]=-0.5;
 //    
 //    ReLatticeVec[0][0]=2.0*Pi/sqrt(3.0);
-//    ReLatticeVec[0][1]=-2.0*Pi;
+//    ReLatticeVec[0][1]=2.0*Pi;
 //    ReLatticeVec[1][0]=4.0*Pi/sqrt(3.0);
 //    ReLatticeVec[1][1]=0.0;
 //    
@@ -85,4 +86,98 @@ Vec<real> Site::GetVec()
         vec += lattice.LatticeVec[i]*Coordinate[i];
     vec += lattice.SubLatticeVec[SubLattice];
     return vec;
+}
+
+Site GetSite(const int& i)
+{
+    int name, layer;
+    Site s;
+    name = i/2;
+    s.SubLattice = i-name*2;
+    
+    for(int j=D-1; j>=0; j--)
+    {
+        layer = 1;
+        for(int k=j-1; k>=0; k--)
+            layer = layer*L[k];
+        s.Coordinate[j] = name/layer;
+        name = name - s.Coordinate[j]*layer;
+    }
+    return s;
+}
+
+/**
+ *  get the real vector for the distance between two sites
+ *
+ *  @return a real vector $\vec{r_2}-\vec{r_1}$
+ */
+Vec<real> Distance::GetVec()
+{
+    Vec<real> vec(0.0);
+    for(int i=0; i<D; i++)
+        vec += lattice.LatticeVec[i]*Dr[i];
+    vec += lattice.SubLatticeVec[SubLattice[1]]-lattice.SubLatticeVec[SubLattice[0]];
+    return vec;
+}
+
+/**
+ *  get a new distance which is symmetric with current distance and within the first quarter of the lattice
+ *
+ *  @return a distance within [0, (L/2)]
+ */
+Distance Distance::Mirror()
+{
+    Distance dis;
+    dis=*this;
+    for(int i=0; i<D; i++)
+        dis.Dr[i] = ::Mirror(Dr[i], L[i]);
+    return dis;
+}
+/**
+ *  get a int with is a symmetric with i within system size L
+ *
+ *  @param i the initial int
+ *  @param L system size
+ *
+ *  @return new variable within [0, L/2] which is symmetric with i
+ */
+int Mirror(int i, const int& L)
+{
+    if(i<0) i=abs(i);
+    if(i>L/2) i = L-i;
+    return i;
+}
+
+/**
+ *  Distance = Site1 - Site2
+ *
+ *  @param i Site1
+ *  @param j Site2
+ *
+ *  @return a new Distance(Site1-Site2)
+ */
+Distance operator-(const Site& i, const Site& j)
+{
+    Distance dis;
+    dis.Dr=i.Coordinate-j.Coordinate;
+    dis.SubLattice[1] = i.SubLattice;
+    dis.SubLattice[0] = j.SubLattice;
+    return dis;
+}
+
+void Lattice::PlotLattice()
+{
+    const unsigned int N=Vol;
+    //save it to file
+    const unsigned int shape[] = {N, (unsigned int)D};
+    real data[N*D];
+    Site s;
+    for(int i; i<N; i++)
+    {
+        s=GetSite(i);
+        for(int j=0; j<D; j++)
+            data[i*D+j]=s.GetVec()[j];
+        std::cout<< data[i*D]<<' '<<data[i*D+1]<<std::endl;
+    }
+    cnpy::npy_save("sq.npy",data,shape,2,"w");
 }
--- src/lattice/lattice.h
+++ src/lattice/lattice.h
@@ -20,25 +20,43 @@ public:
     Vec<int> Coordinate;
     
     Site():SubLattice(0){}
+    Site(const int& i):SubLattice(i){}
     int GetName();
     Vec<real> GetVec();
+    
+    bool operator==(const Site& v2)
+    {
+        if(SubLattice!=v2.SubLattice) return false;
+        if(!(Coordinate==v2.Coordinate)) return false;
+        return true;
+    }
 };
+
+Site GetSite(const int&);
 
 /**
  *  class Distance defines Site1-Site2 using the vector: Dr and sublattice of Site1 and Site2: SubLattice[0] and SubLattice[1].
  */
-//class Distance{
-//public:
-//    int SubLattice[2];
-//    Vec<int> Dr;
-//    
-//    Distance(){}
-//    Distance(const Site&, const Site&);
-//    Vec<real> GetVec() const;
-//    Distance Mirror() const;
-//};
-//
-//Distance operator-(Site S1, Site S2);
+
+class Distance{
+public:
+    int SubLattice[2];
+    Vec<int> Dr;
+    
+    Distance(){}
+    Distance(const Site& i, const Site& j)
+    {
+        Dr=j.Coordinate-i.Coordinate;
+        SubLattice[0]=i.SubLattice;
+        SubLattice[1]=j.SubLattice;
+    }
+    
+    Vec<real> GetVec();
+    Distance Mirror();
+};
+
+Distance operator-(const Site& S1, const Site& S2);
+
 
 /**
  *  class Lattice includes three set of vectors: 1) LatticeVec (unit cell lattice vector); 2)ReLatticeVec (reciprocal lattice vector for k); 3) SubLatticeVec (vectors between different sublattices in the same unit cell).
@@ -50,15 +68,15 @@ public:
     Vec<real> SubLatticeVec[NSublattice];
     
     Lattice() {Initialize();}
-    Site GetNN(const Site&, const int&) const;
-    Site GetNNN(const Site&, const int&) const;
-    void PlotLattice() const;
+    void PlotLattice();
+    
+private:
     void Initialize();
 };
 
 extern Lattice lattice;
 
-int Mirror(const int&, const int&);
+int Mirror(int, const int&);
 int TestLattice();
 
 #endif /* defined(__Fermion_Simulator__lattice__) */
