084e28eb78053f5557b04f5f36d5ce69724526e9
Kun Chen <chenkun@mail.ustc.edu.cn>
2014-10-15 22:23:31 -0400

    estimator part passed tests

--- src/observable/observable_test.cpp
+++ src/observable/observable_test.cpp
@@ -24,27 +24,32 @@ int TestObservable()
 
 void TestObservableIO()
 {
-    EstimateKeeper<Complex> quan("TestQuan");
+    Estimator<Complex> quan1("1");
+    Estimator<Complex> quan2("2");
     Complex a[10];
     for(int i=0;i<10;i++)
     {
         a[i]=Complex(i+1,10-i);
-        quan.AddStatistics(a[i]);
+        quan1.AddStatistics(a[i]);
+        quan2.AddStatistics(-a[i]);
     }
     Estimate<Complex> ExpectedResult(Complex(5.0, 5.0),Complex(1.5, 0.9));
     //!!!This two value only works if you set _norm=1.0 and ThrowRatio=1.0/3
-    sput_fail_unless(Equal(quan.Estimate().Mean, ExpectedResult.Mean),
+    sput_fail_unless(Equal(quan1.Estimate().Mean, ExpectedResult.Mean),
                      "check the Mean value.");
-    sput_fail_unless(Equal(quan.Estimate().Error, ExpectedResult.Error),
+    sput_fail_unless(Equal(quan1.Estimate().Error, ExpectedResult.Error),
                      "check the Error value.");
     
-    //EstimateKeeper IO operation
-    quan.WriteToFile("TestObservable", "w");
-    cnpy::npz_t test=cnpy::npz_load("TestObservable.npz");
-    EstimateKeeper<Complex> quan_another("TestQuan");
-    quan_another.ReadFromFile(test);
-    sput_fail_unless(Equal(quan_another.Estimate().Mean, ExpectedResult.Mean),
+    //Estimator IO operation
+    
+    EstimatorVector<Complex> QuanVector;
+    QuanVector.push_back(quan1);
+    QuanVector.push_back(quan2);
+    QuanVector.SaveState("TestObservable", "w");
+    QuanVector.ClearStatistics();
+    QuanVector.ReadState("TestObservable");
+    sput_fail_unless(Equal(QuanVector[1].Estimate().Mean, -ExpectedResult.Mean),
                      "check the Mean value.");
-    sput_fail_unless(Equal(quan_another.Estimate().Error, ExpectedResult.Error),
+    sput_fail_unless(Equal(QuanVector[1].Estimate().Error, ExpectedResult.Error),
                      "check the Error value.");
 }
