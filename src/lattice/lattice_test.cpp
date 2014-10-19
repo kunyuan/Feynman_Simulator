05700b731dcb6735df52dfe264916e9d70546a04
Kun Chen <chenkun@mail.ustc.edu.cn>
2014-10-19 11:51:10 -0400

    introduce array.h

--- src/lattice/lattice_test.cpp
+++ src/lattice/lattice_test.cpp
@@ -32,7 +32,7 @@ int TestLattice()
 
 void Test_Site()
 {
-    Site s, s1(235);
+    Site s;
     Vec<real> vec;
     vec[0]=5.5;
     vec[1]=7.5;
@@ -40,9 +40,8 @@ void Test_Site()
     s.Coordinate[0]=5;
     s.Coordinate[1]=7;
     s.Sublattice=1;
-    sput_fail_unless(s.GetName()==235, "name of site(B, 5, 5) on 16*32");
-    sput_fail_unless(s.GetVec()==vec, "vector of site(B, 5, 5) on 16*32");
-    sput_fail_unless(s==s1, "Site of name 235");
+    sput_fail_unless(s.GetName()==NSublattice*(L[0]*7+5)+1, "name of site(B, 5, 5)");
+    sput_fail_unless(s.GetVec()==vec, "vector of site(B, 5, 7) ");
 }
 
 void Test_Distance()
@@ -52,14 +51,15 @@ void Test_Distance()
     s1.Coordinate[0] = 3;
     s1.Coordinate[1] = 1;
     
-    s2.Coordinate[0] = 1;
-    s2.Coordinate[1] = 31;
+    s2.Coordinate[0] = 4;
+    s2.Coordinate[1] = 3;
     
     Distance dis(s1, s2);   //s1---->s2, equal to dis=s2-s1;
     
     sput_fail_unless(dis.GetVec()==s2.GetVec()-s1.GetVec(), "distance=site1-site2, vecotr");
     sput_fail_unless(dis.GetSublattice(IN)==s1.Sublattice, "distance=site1-site2, sublattices");
     sput_fail_unless(dis.GetSublattice(OUT)==s2.Sublattice, "distance=site1-site2, sublattices");
-    sput_fail_unless(dis.Mirror().dCoordinate[0]==2 && dis.Mirror().dCoordinate[1]==2, "mirror(distance)");
+    sput_fail_unless(dis.Coordinate()==2*L[0]+1, "distance=site1-site2, sublattices");
+    sput_fail_unless(dis.Mirror().dCoordinate[0]==1 && dis.Mirror().dCoordinate[1]==2, "mirror(distance)");
 }
 
