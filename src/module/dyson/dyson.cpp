e4a9e69e67dfa9264bcadb1c9bdfa16162792315
huangy22 <huangy22@users.noreply.github.com>
2014-11-17 11:22:34 -0500

    add matrix inverse

--- src/module/dyson/dyson.cpp
+++ src/module/dyson/dyson.cpp
@@ -13,6 +13,8 @@
 using namespace dyson;
 using namespace weight;
 
+Complex* Inverse(Complex*);
+
 bool Dyson::BuildNew(para::ParaDyson &para, Weight &weight)
 {
     Beta = para.Beta;
@@ -27,3 +29,24 @@ void Dyson::DeriveG()
 {
     //    G->_Weight();
 }
+
+/**
+ *  2*2 matrix inverse ( 0  1
+ *                       2  3)
+ *
+ *  @param Complex* original matrix
+ *
+ *  @return  the inversed matrix
+ */
+Complex* Inverse(Complex* matrix)
+{
+    Complex denominator = matrix[0]*matrix[3]-matrix[1]*matrix[2];
+    Complex tmp;
+    tmp = matrix[3]/denominator;
+    matrix[3] = matrix[0]/denominator;
+    matrix[0] = tmp;
+    matrix[1] *= -1.0/denominator;
+    matrix[2] *= -1.0/denominator;
+    return matrix;
+}
+
