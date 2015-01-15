475acf76344bf596b96f965394699d3f8755a5fe
Kun Chen <chenkun@mail.ustc.edu.cn>
2015-01-15 18:46:11 -0500

    add message function in calculator

--- calculator/main.py
+++ calculator/main.py
@@ -5,7 +5,7 @@ import lattice as lat
 import collect
 from weight import UP,DOWN,IN,OUT
 from logger import *
-import os, sys, model, weight, parameter, plot, argparse
+import os, sys, model, weight, parameter, plot, argparse, time, traceback
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--PID", help="use PID to find the input file")
@@ -28,69 +28,78 @@ Lat=lat.Lattice(para["Lattice"]["Name"], Map)
 ##########INITIALIZATION ##########################
 Factory=model.BareFactory(Map, para["Model"])
 G0,W0=Factory.Build(para["Model"]["Name"], para["Lattice"]["Name"])
-print "G0=\n", G0.Data[UP,0,UP,0,0,:]
 #Factory.Plot()
+def Measure(G0, W0, G, W, Sigma, Polar):
+    log.info("Measuring...")
+    calc.Check_Denorminator(W0, Polar, Map)
+    ChiTensor = calc.Calculate_ChiTensor(W0, Polar, Map)
+    Chi, _ = calc.Calculate_Chi(ChiTensor, Map)
+    ##########OUTPUT AND FILE SAVE ####################
+    spinUP=Map.Spin2Index(UP,UP)
+    print "Polar=\n", Polar.Data[spinUP,0,spinUP,0,0,:]
+    print "W=\n", W.Data[spinUP,0,spinUP,0,0,:]
+    print "G=\n", G.Data[UP,0,UP,0,0,:]
+    print "Sigma=\n", Sigma.Data[UP,0,UP,0,0,:]
+    print "Polar=\n", Polar.Data[spinUP,0,spinUP,0,0,:]
+    print "Chi=\n", Chi.Data[0,0,0,0,0,:]
+    data={}
+    data["G"]=G.ToDict()
+    data["W"]=W.ToDict()
+    data["W"].update(W0.ToDict())
+    data["Sigma"]=Sigma.ToDict()
+    data["Polar"]=Polar.ToDict()
+    data["Chi"]=Chi.ToDict()
+    IO.SaveBigDict(WeightFile, data)
+    #plot what you are interested in
+    try:
+        plot.PlotSpatial(Chi, Lat, 0, 0)
+    except:
+        pass
 
 if para["Job"]["StartFromBare"] is True or os.path.exists(WeightFile+".pkl") is False:
     #start from bare
     log.info("Start from G0 and W0 to do dyson...")
     G=G0.Copy()
-    for sub in range(2):
-        print G.Data[UP,sub,UP,sub,0,-1]-G.Data[DOWN, sub, DOWN, sub, 0, -1]
     W=weight.Weight("SmoothT", Map, "FourSpins", "Symmetric")
-    #ChiTensor = calc.Calculate_ChiTensor(W0, Polar, Map)
-    #Chi, _=calc.Calculate_Chi(ChiTensor, Map)
-    #plot.PlotSpatial(Chi, Lat, 0, 0)
-    #sys.exit(0)
-
     for i in range(10):
-        log.info("Round {0}...".format(i))
+        log.info("Round #{0}...".format(i))
         Sigma=calc.Sigma_FirstOrder(G, W, Map)
         Sigma0=calc.Sigma0_FirstOrder(G, W0, Map)
         Polar=calc.Polar_FirstOrder(G, Map)
-
         #######DYSON FOR W AND G###########################
         G = calc.G_Dyson(G0, Sigma0, Sigma, Map)
         W = calc.W_Dyson(W0, Polar, Map)
         ###################################################
-        calc.Check_Denorminator(W0, Polar, Map)
+    Measure(G0, W0, G, W, Sigma, Polar)
 
 else:
     #########READ G,SIGMA,POLAR; CALCULATE SIGMA0 #################
-    log.info("Load Sigma/Polar and G to do dyson...")
-    data=IO.LoadBigDict(WeightFile)
-    G=weight.Weight("SmoothT", Map, "TwoSpins", "AntiSymmetric").FromDict(data["G"])
-    MaxOrder=para["Dyson"]["Order"]
-    SigmaMC, PolarMC=collect.CollectStatis(Map, MaxOrder)
-    Sigma,Polar=collect.UpdateWeight(SigmaMC, PolarMC, paraDyson["ErrorThreshold"], paraDyson["OrderAccepted"])
-
-    Sigma0=calc.Sigma0_FirstOrder(G, W0, Map)
-    #######DYSON FOR W AND G###########################
-    G = calc.G_Dyson(G0, Sigma0, Sigma, Map)
-    W = calc.W_Dyson(W0, Polar,Map)
-    ###################################################
-
-######Calculate Chi ###############################
-ChiTensor = calc.Calculate_ChiTensor(W0, Polar, Map)
-Chi, _=calc.Calculate_Chi(ChiTensor, Map)
-
-##########OUTPUT AND FILE SAVE ####################
-spinUP=Map.Spin2Index(UP,UP)
-print "Polar=\n", Polar.Data[spinUP,0,spinUP,0,0,:]
-print "W=\n", W.Data[spinUP,0,spinUP,0,0,:]
-print "G=\n", G.Data[UP,0,UP,0,0,:]
-print "Sigma=\n", Sigma.Data[UP,0,UP,0,0,:]
-print "Polar=\n", Polar.Data[spinUP,0,spinUP,0,0,:]
-print "Chi=\n", Chi.Data[0,0,0,0,0,:]
-
-data={}
-data["G"]=G.ToDict()
-data["W"]=W.ToDict()
-data["W"].update(W0.ToDict())
-data["Sigma"]=Sigma.ToDict()    ####ForTest
-data["Polar"]=Polar.ToDict()    ####ForTest
-data["Chi"]=Chi.ToDict()        ####ForTest
-
-IO.SaveBigDict(WeightFile, data)
-###################################################
-plot.PlotSpatial(Chi, Lat, 0, 0)
+    MessageFile=para["Job"]["MessageFile"]
+    Version=parameter.GetVersion(MessageFile)
+    while True:
+        Version+=1
+        log.info("Start #{0}...".format(Version))
+        try:
+            log.info("Load Sigma/Polar and G to do dyson...")
+            G0,W0=Factory.Build(para["Model"]["Name"], para["Lattice"]["Name"])
+            #reinitialize G0, W0 to kill accumulated error
+            data=IO.LoadBigDict(WeightFile)
+            G=weight.Weight("SmoothT", Map, "TwoSpins", "AntiSymmetric").FromDict(data["G"])
+            MaxOrder=para["Dyson"]["Order"]
+            log.info("Collecting Sigma/Polar statistics...")
+            SigmaMC, PolarMC=collect.CollectStatis(Map, MaxOrder)
+            Sigma,Polar=collect.UpdateWeight(SigmaMC, PolarMC, 
+                    paraDyson["ErrorThreshold"], paraDyson["OrderAccepted"])
+            Sigma0=calc.Sigma0_FirstOrder(G, W0, Map)
+            log.info("Dyson GW...")
+            #######DYSON FOR W AND G###########################
+            G = calc.G_Dyson(G0, Sigma0, Sigma, Map)
+            W = calc.W_Dyson(W0, Polar,Map)
+            ####### Measure ############
+            Measure(G0, W0, G, W, Sigma, Polar)
+            parameter.Broadcast(MessageFile, {"Version": Version, "Beta": Map.Beta})
+            log.info("#{0} is done!".format(Version))
+        except:
+            log.warnings("#{0} fails due to\n {1}".format(Version, traceback.format_exc()))
+        finally:
+            time.sleep(para["Dyson"]["SleepTime"])
