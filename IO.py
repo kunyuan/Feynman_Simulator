41132b74e41da1240928f888a213dff36648868b
Kun Chen <chenkun@mail.ustc.edu.cn>
2015-01-15 18:46:11 -0500

    add message function in calculator

--- IO.py
+++ IO.py
@@ -19,6 +19,7 @@ def LoadDict(filename):
         return eval(f.read())
 
 def SaveBigDict(filename, root):
+    #TODO: better to add an file read-lock here
     if filename[-4:]!=".pkl":
         filename+=".pkl"
     with open(filename, "w") as f:
