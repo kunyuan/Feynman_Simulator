d4ae8959a9b1476b20c6483410cfc1d00a495884
Kun Chen <chenkun@mail.ustc.edu.cn>
2015-02-08 17:38:59 -0500

    clean up

--- _job_MC_1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#PBS -N Feynman_Simulator.MC
-#PBS -o /Users/kchen/project/Feynman_Simulator/Output
-#PBS -e /Users/kchen/project/Feynman_Simulator/Error
-cd /Users/kchen/project/Feynman_Simulator
-/Users/kchen/project/Feynman_Simulator/simulator.exe /Users/kchen/project/Feynman_Simulator/infile/_in_MC_1
