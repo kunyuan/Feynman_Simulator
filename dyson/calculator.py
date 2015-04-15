6f02653abaac314eb4f98b604e642e2e2c6edf94
huangy22 <huangy22@users.noreply.github.com>
2015-04-15 15:10:07 -0400

    np.linalg

--- dyson/calculator.py
+++ dyson/calculator.py
@@ -212,6 +212,6 @@ def Check_Denorminator(Denorm, Determ, map):
     pos=np.where(Determ==Determ.min())
     x,t=pos[0][0], pos[1][0]
     log.info("The minmum {0} is at K={1} and Omega={2}".format(Determ.min(), map.IndexToCoordi(x), t))
-    log.info("The linalg.cond is {0}".format(linalg.cond(np.array(Denorm[:,:,x,t]))))
+    log.info("The linalg.cond is {0}".format(np.linalg.cond(Denorm[:,:,x,t])))
     if Determ.min().real<0.0 and Determ.min().imag<1.0e-4:
         raise DenorminatorTouchZero(Determ.min(), map.IndexToCoordi(x), t)
