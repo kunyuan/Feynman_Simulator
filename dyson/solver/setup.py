7465064346ff8a01f2130a4b1a361fe34f547576
Yuan <huangy22@gmail.com>
2015-07-10 17:28:26 +0800

    compile and setup

--- dyson/solver/setup.py
+++ dyson/solver/setup.py
@@ -2,15 +2,21 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy,os
-os.environ["CC"]="icc"
 
+#use icc and intel mkl lib
+#os.environ["CC"]="icc"
+#ext_modules = Extension("lu_fast",["lu_fast.pyx"],\
+    #include_dirs=[numpy.get_include()],
+    #extra_compile_args=['-O3'],
+    ##extra_link_args=['-fopenmp'],
+    ##libraries=['mkl_rt']
+    #)
+ 
+#use default compiler
 ext_modules = Extension("lu_fast",["lu_fast.pyx"],\
     include_dirs=[numpy.get_include()],
     extra_compile_args=['-O3'],
-    extra_link_args=['-fopenmp'],
-    #extra_link_args=['-llapack']
-    #libraries=['mkl_intel_lp64','mkl_intel_thread','mkl_core']
-    libraries=['mkl_rt']
+    extra_link_args=['-llapack']
     )
  
 setup(
